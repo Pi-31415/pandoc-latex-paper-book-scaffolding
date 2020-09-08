8372e5f2f1f8d6f0c4b4ee3647fb9dac0eee5de7
Pi-31415 <paingthetko.pi@gmail.com>
2020-09-08 21:19:43 +0630

    .

--- documents/lab0.cpp
+++ documents/lab0.cpp
@@ -2,8 +2,7 @@
 /* Name: Pi, Student Number: N13394469 */
 /* Date: Sep 8, 2020. */
 /* Program: distance.cpp */
-/* Description: This program computes the distance */
-/* between two points. */
+/* Description: This program computes the distance between two points. */
 /*-------------------------------------------------*/
 #include <iostream>
 #include <cmath>
@@ -11,7 +10,7 @@ using namespace std;
 int main()
 {
     /* Declare and initialize the variables */
-    double x1,y1,x2,y2,distance;
+    double x1,y1,x2,y2,d;
     cout << "Provide x1: " ;
     cin >> x1;
     cout << "Provide y1: " ;
@@ -21,10 +20,10 @@ int main()
     cout << "Provide y2: " ;
     cin >> y2;
 
-    distance = sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
+    d = sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
 
     /* Print the distance */
-    cout << "The distance between the two points is " << distance << endl;
+    cout << "The distance between the two points is " << d << endl;
     return (0);
 }
 /*--------------------------End---------------------*/
